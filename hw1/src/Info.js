88929fa36b7c94ebf8c7604deb78b7e1248b9f46
elvina22222 <114909756+elvina22222@users.noreply.github.com>
2022-12-15 01:40:57 +0400

    add info

--- hw1/src/Info.js
+++ hw1/src/Info.js
@@ -2,8 +2,14 @@ import React, { Component } from "react";
 export default class Server extends Component {
 const express = require('express');
 const app = express();
+const inf = [
+    [0, 'A' , 21],
+    [1 , 'B' , 45],
+    [2 , 'C' , 67]
+];
+
 app.get('/', function (req, res) {
-    res.send('My name is Elvina');
+    res.send(inf);
   });
   
   app.listen(3000, function () {
