602492ceb1e15138539a702bd8da4a21c6c0a111
elvina22222 <114909756+elvina22222@users.noreply.github.com>
2022-12-15 01:48:51 +0400

    add card n client

--- hw1/src/Server.js
+++ hw1/src/Server.js
@@ -5,6 +5,13 @@ const app = express();
 app.get('/', function (req, res) {
     res.send('My name is Elvina');
   });
+
+  app.get('/card', function (req, res) {
+    res.send('Этот роут отвечает за карты');
+  });
+  app.get('/client', function (req, res) {
+    res.send('Этот роут отвечает за клиентов');
+  });
   
   app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
